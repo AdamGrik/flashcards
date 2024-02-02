e6b73c24e81b1ec77d723a03b8eca7777e78ec2a
Adam Grik <grik.adam@gmail.com>
2024-02-02 17:05:03 +0100

    spatna vazba otazky prerobena

--- src/oragnisms/EndOfExam/EndOfExam.tsx
+++ src/oragnisms/EndOfExam/EndOfExam.tsx
@@ -18,6 +18,7 @@ const endOfExam = (props: ExamProps) => {
   const handleClick = () => {
     navigate("/");
   };
+
   return (
     <div className="fc-end-of-exam">
       <div className="fc-end-of-exam-score">
@@ -33,29 +34,52 @@ const endOfExam = (props: ExamProps) => {
             question.answer === question.selected
               ? "fc-end-of-exam-question-correct"
               : "fc-end-of-exam-question-incorrect";
-          return (
+
+          return question && question.questionNumber !== undefined ? (
             <div className={"fc-end-of-exam-question " + answerClass}>
               <div className="fc-end-of-exam-question-question">
-                {question.question}
+                {question.questionNumber + 1}. {question.question}
               </div>
               {question.image !== undefined ? (
                 <div
                   className={`fc-question-img fc-image-${question.image}`}></div>
               ) : null}
               <div className="fc-end-of-exam-question-options">
-                {question.options.map((option, index) => (
-                  <div key={index} className="fc-end-of-exam-question-option">
-                    <span className="option-label">
-                      {String.fromCharCode(97 + index)}.){" "}
-                    </span>
-                    {option}
-                  </div>
-                ))}
+                {question.options.map((option, index) => {
+                  if (option === question.answer) {
+                    return (
+                      <div
+                        key={index}
+                        className="fc-end-of-exam-question-correct-answer">
+                        <span>{String.fromCharCode(97 + index)}.) </span>
+                        {option}
+                        <div className="correct-answer">Správna odpoveď</div>
+                      </div>
+                    );
+                  } else if (option === question.selected) {
+                    return (
+                      <div
+                        key={index}
+                        className="fc-end-of-exam-question-selected-answer">
+                        <span>{String.fromCharCode(97 + index)}.) </span>
+                        {option}
+                        <div className="your-answer">Vaša odpoveď</div>
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div
+                        key={index}
+                        className="fc-end-of-exam-question-option">
+                        <span>{String.fromCharCode(97 + index)}.) </span>
+                        {option}
+                      </div>
+                    );
+                  }
+                })}
               </div>
-              <div>Vaša odpoveď: {question.selected}</div>
-              <div>Správna odpoveď: {question.answer}</div>
             </div>
-          );
+          ) : null;
         })}
       </div>
     </div>
