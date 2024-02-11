bd9751ed8d50f400b62cbc98bf808a38abe9b7c2
Adam Grik <grik.adam@gmail.com>
2024-02-11 15:18:37 +0100

    redizajn footeru

--- src/molecules/Footer/Footer.tsx
+++ src/molecules/Footer/Footer.tsx
@@ -4,14 +4,21 @@ import "./Footer.scss";
 const Cards = () => {
   return (
     <div className="fc-footer">
-      <div>Autori: Adam Grík, Adam Petrík</div>
-      <div>Kozultantka: Mgr. Kamila Švecová</div>
-      <div>Školský rok: 2023/2024</div>
-      <div>Trieda: IV.EA</div>
-
-      <a href="https://github.com/AdamGrik/flashcards" target="blank">
-        <div className="fc-github-pic"></div>
-      </a>
+      <div className="fc-footer-left">
+        <div>
+          <div>Autori: Adam Grík, Adam Petrík</div>
+          <div>Kozultantka: Mgr. Kamila Švecová</div>
+        </div>
+        <div>
+          <div>Školský rok: 2023/2024</div>
+          <div>Trieda: IV.EA</div>
+        </div>
+      </div>
+      <div className="fc-footer-right">
+        <a href="https://github.com/AdamGrik/flashcards" target="blank">
+          <div className="fc-github-pic"></div>
+        </a>
+      </div>
     </div>
   );
 };
