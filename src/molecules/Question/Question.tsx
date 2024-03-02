d131f1a516639e5187c78a7d93974b9197b9890e
Adam Grik <grik.adam@gmail.com>
2024-03-02 22:35:51 +0100

    dizajn uprava Question

--- src/molecules/Question/Question.tsx
+++ src/molecules/Question/Question.tsx
@@ -31,19 +31,22 @@ const Question = (props: QuestionProps) => {
   return data && data.questionNumber !== undefined ? (
     <div className="fc-question">
       <div className="fc-question-border">
-        <div className="fc-question-top">
+        <div className="fc-question-question">
           {data.questionNumber + 1}. / {data.totalQuestions} {data.question}
         </div>
         <div>{data.text}</div>
-        <div className="fc-text-question"> {data.textQuestion}</div>
+        <div className="fc-question-text"> {data.textQuestion}</div>
         {data.image !== undefined ? (
           <div className={`fc-question-img fc-image-${data.image}`} />
         ) : null}
 
         {data.options.map((option, index) => (
-          <div key={index} className="fc-question-bottom">
+          <div
+            key={index}
+            className="fc-question-options"
+            onClick={() => setSelectedOption(option)}>
             <input
-              className="fc-question-radio"
+              className="fc-question-options-radio"
               type="radio"
               value={option}
               checked={selectedOption === option}
@@ -51,23 +54,25 @@ const Question = (props: QuestionProps) => {
                 setSelectedOption(e.target.value);
               }}
             />
-            <span className="fc-option-abcd">
-              {String.fromCharCode(97 + index)}.){" "}
-            </span>{" "}
+            <span className="fc-question-options-abcd">
+              {String.fromCharCode(97 + index)}.)
+            </span>
             {option}
           </div>
         ))}
-        <Pagination
-          totalPages={data.totalQuestions ?? 0}
-          selected={data.questionNumber}
-          isFinalQuestion={isFinalQuestion}
-          onPageSelect={(selectedPage) =>
-            onQuestionChange(
-              selectedOption,
-              data.questionNumber ?? 0,
-              selectedPage
-            )
-          }></Pagination>
+        <div className="fc-question-pagination">
+          <Pagination
+            totalPages={data.totalQuestions ?? 0}
+            selected={data.questionNumber}
+            isFinalQuestion={isFinalQuestion}
+            onPageSelect={(selectedPage) =>
+              onQuestionChange(
+                selectedOption,
+                data.questionNumber ?? 0,
+                selectedPage
+              )
+            }></Pagination>
+        </div>
       </div>
     </div>
   ) : null;
